a3db84831058d81b40f601794ce2c8fae2a053ec
Ilham Hidayatulloh <114216025+Ilhamhidayatulloh59@users.noreply.github.com>
2023-05-30 09:16:51 +0700

    TDD

--- ternary.js
+++ ternary.js
@@ -2,8 +2,7 @@ let age = -1
 
 age >= 17 ? console.log("cukup") : age < 17 && age >= 0 ? console.log("kurang") : console.log("blom lahir")
 
-let square = (a, b) => {
-    
-}
+let number = 2
 
-console.log(square(10, 2))
+console.log(Boolean(!!!!!!!!!!!Boolean([])) + true)
+ 
